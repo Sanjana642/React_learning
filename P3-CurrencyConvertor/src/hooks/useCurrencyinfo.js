37bb25e59107c77564c54f3ee2a8ebe46ba9ef56
Sanjana Pisal <sanjanapisal02@gmail.com>
2026-01-28 14:55:50 +0530

    currency convert flow completed

--- P3-CurrencyConvertor/src/hooks/useCurrencyinfo.js
+++ P3-CurrencyConvertor/src/hooks/useCurrencyinfo.js
@@ -4,7 +4,7 @@ function useCurrencyInfo (currency){
     const [data, setData] = useState({});
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())  
         .then((res) => setData(res[currency])) //[currency] is key, you can write res.currency also as setdata is storing this data 
         console.log(data); //check the data if you want to
